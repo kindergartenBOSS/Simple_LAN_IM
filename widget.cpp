92b53ed975b7f5c427334798d5923c1bbb336db2
kindergartenBOSS <34027170+kindergartenBOSS@users.noreply.github.com>
2018-10-23 20:43:31 +0800

    UDP
    UDP_框架

--- widget.cpp
+++ widget.cpp
@@ -1,12 +1,61 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QUdpSocket>
+#include <QHostInfo>
+#include <QMessageBox>
+#include <QScrollBar>
+#include <QDateTime>
+#include <QNetworkInterface>
+#include <QProcess>
 
-Widget::Widget(QWidget *parent) :
+
+Widget::Widget(QWidget *parent, QString username) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    //创建UDP套接字并进行初始化，端口默认设为23232，使用connect语句将其与processPendingDatagrams()槽函数绑定，随时接受来自其他用户的UDP广播消息。
+    uName = username;
+    udpSocket = new QUdpSocket(this);
+    port = 23232;
+    udpSocket->bind(port,QUdpSocket::ShareAddress | QUdpSocket::ReuseAddressHint);
+    connect(udpSocket, SIGNAL(readyRead()), this, SLOT(processPendingDatagrams()));
+    sendMsg(NewUser);//使用这个函数来广播用户登录信息（发送各种UDP数据）
 }
+
+
+void Widget::sendMsg(MsgType type, QString srvaddr)
+{
+    QByteArray data;
+    QDataStream out(&data, QIODevice::WriteOnly);               //
+    QString localHostName = QHostInfo::localHostName();         //获取主机名
+    QString address = getIP();                                  //获取IP地址
+    out << type << getUser() << localHostName;                                    //
+
+    switch (type)
+    {
+    case Msg:
+
+        break;
+    case NewUser:
+
+        break;
+    case UserLeft:
+
+        break;
+    case FileName:
+
+        break;
+    case Refuse:
+
+        break;
+    }
+
+    udpSocket->writeDatagram(data, data.length(), QHostAddress::Broadcast, post);
+}
+
+
 
 Widget::~Widget()
 {
--- widget.h
+++ widget.h
@@ -3,9 +3,14 @@
 
 #include <QWidget>
 
+class QUdpSocket;
+
 namespace Ui {
 class Widget;
 }
+
+//枚举变量标志信息类型，分别为、聊天信息、新用户加入、用户退出、文件名、拒绝接收文件
+enum MsgType{Msg, NewUser, UserLeft, FileName, Refuse};
 
 class Widget : public QWidget
 {
@@ -13,10 +18,27 @@ class Widget : public QWidget
 
 public:
     explicit Widget(QWidget *parent = nullptr);
-    ~Widget();
+    ~Widget();  
+
+protected:
+    void newUser(QString username, QString ipaddr);     //处理新用户加入
+    void userLeft(QString username, QString time);      //处理用户离开
+    void sendMsg(MsgType type, QString srvaddr="");     //广播UDP消息
+
+    QString getIP();                                    //获取IP地址
+    QString getUser();                                  //获取用户名
+    QString getMsg();                                   //获取聊天消息
+
 
 private:
     Ui::Widget *ui;
+    QUdpSocket *udpSocket;
+    qint16 port;
+    QString uName;//
+
+
+private slots:
+    void processPendingDatagrams();                     //接收UDP消息
 };
 
 #endif // WIDGET_H
